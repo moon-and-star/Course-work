8445ff80f6dc73835a1e10ea184f676d4506c7a8
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-01 22:37:05 +0300

    bugfix

--- prepare_data_only.sh
+++ prepare_data_only.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-source ~/anaconda3/bin/activate
+# source ~/anaconda3/bin/activate
 printf "PREPROCESSING DATA\n\n"
 ./preprocess.py
+# source ~/anaconda3/bin/deactivate
+
 printf "CREATING LMDB\n\n"
 ./create_lmdb.sh
-source ~/anaconda3/bin/deactivate
+
 
 #this script imports caffe which doesn't work with python3
