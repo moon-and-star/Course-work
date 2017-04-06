b3075e79ed19ff097f7d3ed50c0ae38124b32e31
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-07 00:54:33 +0300

    new function: fc(). activation parameter included

--- gen_commitee.py
+++ gen_commitee.py
@@ -73,37 +73,58 @@ def maxpool(name, bottom, kernel_size = 2, stride = 2):
 def avepool(name, bottom, kernel_size = 2, stride = 2):
     return L.Pooling(bottom, kernel_size = kernel_size, stride = stride, pool = P.Pooling.AVE, name = name)
 
-def fc_relu(name, bottom, num_output):
+# def fc_relu(name, bottom, num_output):
+#     fc = L.InnerProduct(
+#         bottom,
+#         num_output = num_output,
+#         weight_filler = dict(type = 'xavier'),
+#         name = "{}_{}".format(name, num_output)
+#     )
+#     return fc, L.ReLU(fc, in_place = True, name = "{}_relu".format(name))
+
+
+# def fc_stanh(name, bottom, num_output):
+#     # fc = L.OLOLO(
+#     fc = L.InnerProduct(
+#         bottom,
+#         num_output = num_output,
+#         weight_filler = dict(type = 'xavier'),
+#         name = "{}_{}".format(name, num_output)
+#     )
+
+#     scale1 = L.Scale(fc, in_place = True, name = "{}_prescale".format(name),
+#                      param=dict(lr_mult=0, decay_mult=0),
+#                      scale_param=dict(filler=dict(value=0.6666))
+#                      )
+#     tanh =  L.TanH(scale1, in_place = True, name = "{}_sTanH".format(name))
+#     scale2 = L.Scale(tanh, in_place = True, name = "{}_postscale".format(name),
+#                      param=dict(lr_mult=0, decay_mult=0),
+#                      scale_param=dict(filler=dict(value=1.7159))
+#                      )
+
+#     return fc, scale2
+def fc(name, bottom. num_output, activ="relu"):
     fc = L.InnerProduct(
         bottom,
         num_output = num_output,
         weight_filler = dict(type = 'xavier'),
         name = "{}_{}".format(name, num_output)
     )
-    return fc, L.ReLU(fc, in_place = True, name = "{}_relu".format(name))
 
+    if activ=="relu":
+        return fc, L.ReLU(fc, in_place = True, name = "{}_relu".format(name))
+    elif activ=="scaled_tanh":
+        scale1 = L.Scale(fc, in_place = True, name = "{}_prescale".format(name),
+                         param=dict(lr_mult=0, decay_mult=0),
+                         scale_param=dict(filler=dict(value=0.6666))
+                         )
+        tanh =  L.TanH(scale1, in_place = True, name = "{}_sTanH".format(name))
+        scale2 = L.Scale(tanh, in_place = True, name = "{}_postscale".format(name),
+                         param=dict(lr_mult=0, decay_mult=0),
+                         scale_param=dict(filler=dict(value=1.7159))
+                         )
 
-def fc_stanh(name, bottom, num_output):
-    # fc = L.OLOLO(
-    fc = L.InnerProduct(
-        bottom,
-        num_output = num_output,
-        weight_filler = dict(type = 'xavier'),
-        name = "{}_{}".format(name, num_output)
-    )
-
-    scale1 = L.Scale(fc, in_place = True, name = "{}_prescale".format(name),
-                     param=dict(lr_mult=0, decay_mult=0),
-                     scale_param=dict(filler=dict(value=0.6666))
-                     )
-    tanh =  L.TanH(scale1, in_place = True, name = "{}_sTanH".format(name))
-    scale2 = L.Scale(tanh, in_place = True, name = "{}_postscale".format(name),
-                     param=dict(lr_mult=0, decay_mult=0),
-                     scale_param=dict(filler=dict(value=1.7159))
-                     )
-
-    return fc, scale2
-
+        return fc, scale2
 
 
 def dropout(name, bottom, dropout_ratio):
@@ -156,8 +177,8 @@ def ConvPoolAct(n, net_num, activ):
     ker_sz = [7, 4, 4]
 
     for j in range(3):
-        p_name = "pool{}_{}".format(j+1, net_num)
-        c_name="conv{}_{}".format(j+1, net_num)
+        p_name = "pool{}_{}".format(j, net_num)
+        c_name="conv{}_{}".format(j, net_num)
         conv = conv1(n=n, name=c_name, 
                      bottom=cbott[j], num_output=out_num[j], 
                      kernel_size = ker_sz[j], 
@@ -166,20 +187,23 @@ def ConvPoolAct(n, net_num, activ):
         n[p_name]=pool
         cbott += [pool]
 
+def FcDropAct(n=n, classes=num_of_classes, activ=activ):
+    n.fc4_300, n.relu4 = fc_relu("fc4_{}".format(i), n.pool3, num_output = 300)
+    n.drop4 = dropout("drop4_{}".format(i), n.relu4, dropout_ratio=0.4)
+    n.fc5_classes, relu5 = fc_relu("fc5_{}".format(i), n.relu4, num_output = num_of_classes)
+ 
+
 
 def make_net(n, num_of_classes = 43, activ="relu"):
     for i in range(1):
-        ConvPoolAct(n=n, net_num=i, activ=activ)
-        
+        ConvPoolAct(n=n, net_num=i , activ=activ)
+        FcDropAct(n=n, classes=num_of_classes, activ=activ)
        
         # if activ=="relu":
         # #     n.fc4_300, n.relu4 = fc_relu("fc4_{}".format(i), n.pool3, num_output = 300)
         # #     n.drop4 = dropout("drop4_{}".format(i), n.relu4, dropout_ratio=0.4)
         # #     n.fc5_classes, relu5 = fc_relu("fc5_{}".format(i), n.relu4, num_output = num_of_classes)
-        #     d["fc4_{}__300".format(i)], d["relu4_{}".format(i)] = fc_relu("fc4_{}".format(i), d["pool3_{}".format(i)], num_output = 300)
-        #     d["drop4_{}".format(i)] = dropout("drop4_{}".format(i), n.relu4, dropout_ratio=0.4)
-        #     d["fc5_{}__classes".format(i)], relu5 = fc_relu("fc5_{}".format(i), n.relu4, num_output = num_of_classes)
-
+      
         # elif activ == "scaled_tanh":
         #     n.fc4_300, n.stanh4 = fc_stanh("fc4_{}".format(i), d["pool1_{}".format(i)], num_output = 300)
         #     n.drop4 = dropout("drop4_{}".format(i), n.stanh4, dropout_ratio=0.4)
