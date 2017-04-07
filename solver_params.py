d31e0ab924e6edd171bf7d068064719f4e52ada9
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-07 20:10:59 +0300

    removed solver.prototxt generator

--- solver_params.py
+++ solver_params.py
@@ -1,4 +1,4 @@
-from math import ceil
+
 import argparse
 from util import safe_mkdir, gen_parser
 
@@ -98,52 +98,4 @@ class SolverParameters(object):
                           lr=lr, gamma=gamma
                           )
 
-
-
-
-def get_dataset_size(path=None, dataset='', phase='', mode=''):
-    # path parameter has greater priority
-    if path == None:
-        with open("../local_data/{}/{}/{}_size.txt".format(dataset, mode, phase)) as f:
-             return int(f.read()) # we assume that the file contains only 1 integer value written as a string
-    else:
-         with open(path) as f:
-             return int(f.read()) # we assume that the file contains only 1 integer value written as a string
-
-       
-
-
-def gen_solver(dataset, mode, args):
-
-    train_size = get_dataset_size(dataset=dataset, phase="train", mode=mode)
-    test_size = get_dataset_size(dataset=dataset, phase="test", mode=mode) 
-
-    test_iter = ceil(test_size / float(args.batch_size))
-    epoch_sz = ceil(train_size / float(args.batch_size))
-
-
-    directory = "{}/experiment_{}/{}/{}".format(args.proto_pref,  args.EXPERIMENT_NUMBER, dataset, mode)
-    train_path = "{}/train.prototxt".format(directory)
-    test_path = "{}/test.prototxt".format(directory)
-
-
-    print("Generating solver")
-    print("{} {}\n".format(dataset, mode))     
-    safe_mkdir('{}/{}/{}/'.format(args.proto_pref,dataset,mode))
-    snap_pref = "{}/experiment_{}/{}/{}/snap".format(args.snap_pref,args.EXPERIMENT_NUMBER, dataset, mode) 
-
-    
-
-
-    p = SolverParameters(train_net_path=train_path, test_net_path=test_path, test_iter=test_iter, lr=args.learning_rate,
-                         train_epoch_sz=epoch_sz, n_epoch=args.epoch, test_epoch=args.test_frequency,
-                         snap_pref=snap_pref, snap_epoch=args.snap_epoch, step_epoch=args.step_epoch, gamma=args.gamma)  
-
-
-
-    
-    with open('{}/solver.prototxt'.format(directory), 'w') as f:
-        f.write(p.solvet_txt) 
-        print(p.solvet_txt)
-    
 
