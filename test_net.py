ee243eb9610db4501a68abd1b844fd2aa9e219df
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-12 02:37:12 +0300

    bugfix

--- test_net.py
+++ test_net.py
@@ -8,12 +8,11 @@ sys.path.append('/opt/caffe/python/')
 
 import caffe
 
-net = caffe.Classifier('./Prototxt/experiment_6/rtsd-r1/imajust/trial_1/test.prototxt',
-                './snapshots/experiment_6/rtsd-r1/imajust/trial_1/snap_iter_2250.caffemodel', image_dims=(54, 54))
-# net = caffe.Net('./Prototxt/experiment_6/rtsd-r1/imajust/trial_1/test.prototxt',
-#                 './snapshots/experiment_6/rtsd-r1/imajust/trial_1/snap_iter_2250.caffemodel', caffe.TEST)
+
+net = caffe.Net('./Prototxt/experiment_6/rtsd-r1/imajust/trial_1/test.prototxt',
+                './snapshots/experiment_6/rtsd-r1/imajust/trial_1/snap_iter_2250.caffemodel', caffe.TEST)
 out = net.forward()
-#print(net.blobs["fc5_classes"].data)
+print(net.blobs["softmax"].data)
 #print(net.blobs["loss"].data)
-print(net.predict())
+# print(net.predict())
                      
