2dc7c7d835b5beb667201176d77a2312bd01e96e
Ekaterina Amelina <tel.moon.and.star@gmail.com>
2017-04-12 03:29:36 +0300

    bugfix

--- test_net.py
+++ test_net.py
@@ -9,10 +9,16 @@ sys.path.append('/opt/caffe/python/')
 import caffe
 
 
-net = caffe.Net('./Prototxt/experiment_6/rtsd-r1/imajust/trial_1/test.prototxt',
-                './snapshots/experiment_6/rtsd-r1/imajust/trial_1/snap_iter_2250.caffemodel', caffe.TEST)
+net = caffe.Net('./Prototxt/experiment_9/rtsd-r1/orig/trial_1/test.prototxt',
+                './snapshots/experiment_9/rtsd-r1/orig/trial_1/snap_iter_2500.caffemodel', caffe.TEST)
 out = net.forward()
-print(net.blobs["softmax"].data)
+print (net.blobs)
+#print(net.blobs["softmax"].data)
+print(net.blobs["accuracy_1"].data)
+
+#print(net.blobs["fc5_classes_fc5_67_0_split_0"].data)
+#print(net.blobs["fc5_classes_fc5_67_0_split_1"].data)
+#print(net.blobs["fc5_classes_fc5_67_0_split_2"].data)
 #print(net.blobs["loss"].data)
 # print(net.predict())
                      
