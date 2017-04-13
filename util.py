8d4dc6452931025052c74021bc525819783d7518
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-14 02:45:41 +0300

    bugfix + dropout params

--- util.py
+++ util.py
@@ -56,7 +56,7 @@ def gen_parser():
 
     parser.add_argument('-cg','--conv_group',default=1, type=int, 
                         help='how many groups of filters within conv layers (default = 1 )')
-    parser.add_argument('-tn','--trial_number',default=5, type=int, 
+    parser.add_argument('-tn','--trial_number',default=1, type=int, 
                         help='number of trial for 1 net (default = 5 )')
 
     parser.add_argument('-dr','--drop_ratio',default=0.5, type=float, 
