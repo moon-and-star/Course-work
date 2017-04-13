d2d4ea91585adfc339479ef755c4b9f2fc941076
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-14 00:55:16 +0300

    changed parameters for data layer mean is now before scale scale: 255 -> 128

--- net_generator_exp_num.py
+++ net_generator_exp_num.py
@@ -128,9 +128,9 @@ def initWithData(lmdb, phase, batch_size, mean_path):
     mean = load_image_mean(mean_path)
 
     if mean is not None:
-        transform_param = dict(mirror=False, crop_size = 48, scale=1.0/255, mean_value = map(int, mean))
+        transform_param = dict(mirror=False, crop_size = 48, mean_value = map(int, mean), scale=1.0/128)
     else:
-        transform_param = dict(mirror=False, crop_size = 48, scale=1.0/255)
+        transform_param = dict(mirror=False, crop_size = 48, scale=1.0/128)
 
     if phase == "train":
         PHASE = "TRAIN"
