d2de197e2dc1b296d91aa245a267dc2bb4a6c244
Ekaterina Amelina <tel.moon.and.star@gmail.com>
2017-04-14 04:19:45 +0300

    script for rtsd-r1 orig

--- logs/experiment_10/rtsd-r1/orig/train.sh
+++ logs/experiment_10/rtsd-r1/orig/train.sh
@@ -13,7 +13,7 @@ echo " tools = ${TOOLS}"
 
 
 EXPERIMENT_NUM=10    
-GPU_NUM=0   
+GPU_NUM=2   
 BATCH_SZ=1024    
 EPOCH=100     
 TEST_FR=1     
@@ -21,7 +21,9 @@ SNAP_FR=10
 STEP_FR=20     
 GAMMA=0.5     
 LR=1e-3   
-activation=relu  
+activation=relu
+drop=false
+drop_ratio=0.5
 CONV_GROUP=1
 TRY_NUM=5
 
@@ -38,6 +40,8 @@ msg="""
 	GAMMA=$GAMMA     
 	LR=$LR   
 	activation=$activation  
+	drop=$drop
+	drop_ratio=$drop_ratio
 	CONV_GROUP=$CONV_GROUP
 	TRY_NUM=$TRY_NUM"""
 
@@ -50,7 +54,7 @@ datasets=("rtsd-r1")
 # modes=("histeq" "imajust")
 
 
-# datasets=("rtsd-r1" "rtsd-r3")
+datasets=("rtsd-r1" "rtsd-r3")
 modes=("CoNorm" "orig" "AHE" "histeq" "imajust")
 
 
@@ -60,10 +64,18 @@ printf "\n\n GENERATING ARCHITECTURES\n\n"
 for k in $(seq 1 $TRY_NUM); do
 	snap_pref="./snapshots"
 	proto_pref="./Prototxt"
-	python2 ./net_generator_exp_num.py \
-			$EXPERIMENT_NUM -b $BATCH_SZ -e $EPOCH -tf $TEST_FR -sn $SNAP_FR \
-			-st $STEP_FR  -lr $LR -g $GAMMA -a $activation \
-			-cg $CONV_GROUP -s $snap_pref -p $proto_pref -tn $k
+
+	if [  "$drop" == "true" ]; then
+		python2 ./net_generator_exp_num.py \
+				$EXPERIMENT_NUM -b $BATCH_SZ -e $EPOCH -tf $TEST_FR -sn $SNAP_FR \
+				-st $STEP_FR  -lr $LR -g $GAMMA -a $activation \
+				-cg $CONV_GROUP -s $snap_pref -p $proto_pref -tn $k -d -dr $drop_ratio
+	elif [ "$drop" == "false" ]; then
+		python2 ./net_generator_exp_num.py \
+				$EXPERIMENT_NUM -b $BATCH_SZ -e $EPOCH -tf $TEST_FR -sn $SNAP_FR \
+				-st $STEP_FR  -lr $LR -g $GAMMA -a $activation \
+				-cg $CONV_GROUP -s $snap_pref -p $proto_pref -tn $k 
+	fi
 done
 
 
