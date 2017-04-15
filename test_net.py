ad14139507f593ae796a4b5c1386a267ada22244
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-15 18:40:40 +0300

    trying to compyte on GPU

--- test_net.py
+++ test_net.py
@@ -3,14 +3,15 @@
 #!/usr/bin/env python
 
 from gen_solver import get_dataset_size
-
+import math
 import sys
 sys.path.append('/opt/caffe/python/')
 
 
 import caffe
-import math
 
+caffe.set_mode_gpu()
+caffe.set_device(3)
 
 # def test():
 # 	size = get_dataset_size(dataset="rtsd-r1", phase="test", mode="orig")
@@ -60,18 +61,26 @@ def test():
 	net = d["net"]
 	batch_size = d["batch_size"]
 
+	# sum = 0
+	# n = math.ceil(size*1.0 / batch_size)
+	# for i in range (int(n)):
+	# 	print("batch in proccess: {}".format(i))
+	# 	out = net.forward()
+	# 	acc =net.blobs["accuracy_1"].data
+	# 	print(acc)
+	# 	if i < n-1:
+	# 		sum += acc * batch_size
+	# 	else:
+	# 		sum += acc * (size - batch_size * (n-1))
+
 	sum = 0
-	n = math.ceil(size*1.0 / batch_size)
-	for i in range (int(n)):
-		print("batch in proccess: {}".format(i))
+	for i in range (size):
+		if i % 100 == 0:
+			print("image in proccess: {}".format(i))
 		out = net.forward()
 		acc =net.blobs["accuracy_1"].data
-		print(acc)
-		if i < n-1:
-			sum += acc * batch_size
-		else:
-			sum += acc * (size - batch_size * (n-1))
-
+		# print(acc)
+		sum += acc
 	print("average = {}".format(sum / size))
 
 
