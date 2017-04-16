a2a6f7d14f321ef11deee1dd9d6b12a8ce938c79
Ekaterina <tel.moon.and.star@gmail.com>
2017-04-16 23:35:12 +0300

    bugfix

--- test_net.py
+++ test_net.py
@@ -139,6 +139,7 @@ def TestCommitee(exp_num, dataset):
     correct = 0
     with open('{}/gt_{}.txt'.format(rootpath, phase), 'r') as f:
         for image_name,clid in [x.replace('\n', '').split(' ') for x in f]:
+            clid = int(clid)
             total +=1
             prepare(net, rootpath, phase, image_name)
             out = net.forward()
