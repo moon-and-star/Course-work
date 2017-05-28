d2c533aff9a195129a80dbcfeb0e946d6b189126
Ekaterina <tel.moon.and.star@gmail.com>
2017-05-28 20:41:20 +0300

    bugfix

--- reduced_gt.py
+++ reduced_gt.py
@@ -1,29 +1,35 @@
 #!/usr/bin/env python
 import argparse
 
-if __name__ == '__main__':
+def init():
     parser = argparse.ArgumentParser()
     parser.add_argument("th",type=int, 
                         help='threshold for number of signs in one class')
     args = parser.parse_args()
+    return args
 
-    threshold = args.th
 
-    rootpath = '../global_data/Traffic_signs/RTSD/classification'
-    class_size = [0] * 116
-    for phase in ["train", "test"]:
-        with open("{}/gt_{}_full.txt".format(rootpath, phase)) as f:
-            for line in f.readlines():
-                s = line.split("/")[0]
-                class_size[int(s)] += 1
+if __name__ == '__main__':
+    threshold = init().th
 
-    print(class_size)
-    for i in range(len(class_size)):
-        print(i, class_size[i])
-    for phase in ["train", "test"]:
-        with open("{}/gt_{}_full.txt".format(rootpath, phase)) as f:
-            with open("{}/gt_{}.txt".format(rootpath, phase), 'w') as out:
+    rootpath = '../local_data/RTSD/'
+    modes =  modes = ["orig", "histeq", "AHE", "imajust", "CoNorm" ]
+    for mode in modes:
+        class_size = [0] * 116
+        for phase in ["train", "test"]:
+            with open("{}/{}/gt_{}_full.txt".format(rootpath, mode, phase)) as f:
                 for line in f.readlines():
                     s = line.split("/")[0]
-                    if class_size[int(s)] >= threshold:
-                        out.write(line)
+                    class_size[int(s)] += 1
+
+        print(class_size)
+        for i in range(len(class_size)):
+            print(i, class_size[i])
+
+        for phase in ["train", "test"]:
+            with open("{}/{}/gt_{}_full.txt".format(rootpath, mode, phase)) as f:
+                with open("{}/{}/gt_{}.txt".format(rootpath, mode,  phase), 'w') as out:
+                    for line in f.readlines():
+                        s = line.split("/")[0]
+                        if class_size[int(s)] >= threshold:
+                            out.write(line)
