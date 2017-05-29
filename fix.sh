fcebe8cf7a994a383a2a89cc8cee5e50e98a0cef
Ekaterina <tel.moon.and.star@gmail.com>
2017-05-29 12:05:48 +0300

    changes names of snapshots from 22 exp

---
+++ fix.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+modes=(  "CoNorm" "AHE" "histeq" "imajust" "orig")
+for j in "${modes[@]}"
+do
+	for k in $(seq 1 $TRY_NUM); do
+		mv ./snapshots/experiment_22/RTSD/$j/trial_$k/snap_iter_2550.caffemodel \
+			./snapshots/experiment_22/RTSD/$j/trial_$k/snap_iter_2070.caffemodel
+		mv ./snapshots/experiment_22/RTSD/$j/trial_$k/snap_iter_2550.solverstate \
+			./snapshots/experiment_22/RTSD/$j/trial_$k/snap_iter_2070.solverstate
+	done
+done
